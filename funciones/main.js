185bd17359116b5df60e17a8b16043917640c440
Silvia Ledesma <leo_alva97@hotmail.com>
2024-02-13 19:50:08 -0300

    creacion de proyecto

---
+++ funciones/main.js
@@ -0,0 +1,160 @@
+
+/*
+//EJERCICIO 1
+function sumar(){
+    console.log(100*20)
+}*/
+
+
+/* 
+//EJERCICIO 2
+ function sumar(){
+    console.log(100*20)
+}
+
+ sumar()*/
+
+ //EJERCICIO 3
+ /*function sumar(x,y){
+    console.log(x,y)
+ }
+
+ sumar(1,3)
+*/
+
+ //ejercicio 4
+/*let numeroA = 4
+let numeroB = 5
+
+function sumar(x,y){
+    console.log(x + y)
+}
+
+sumar(numeroA , numeroB)
+*/
+
+//ejercicio 5
+/*let numeroA = 100
+let numeroB = 20
+
+function sumar(x,y){
+    let resultado = x * y
+    return resultado;
+}
+
+let resultado = sumar(numeroA, numeroB)
+console.log(resultado)*/
+
+/*let numeroA = 100
+let numeroB = 20
+
+var suma = function(a, b) {
+    return a * b;
+  };
+  
+  var resultado = suma(numeroA, numeroB);
+  console.log(resultado);
+*/
+
+/*let numeroA = 100
+let numeroB = 20
+
+const suma = (a,b) => a*b;
+
+const resultado = suma (numeroA , numeroB)
+console.log(resultado)*/
+
+/*let nombre = "jorge"
+
+function saludar (nombre){
+    return nombre
+}
+
+let saludo = saludar(nombre)
+console.log("hola mi nombre es" + saludo)*/
+
+
+/*function multiplicar(a,b){
+    return a*b
+}
+
+let resultado = multiplicar (5,2)
+console.log(resultado)*/
+
+/*function area (base ,altura){
+    return base * altura /2
+}
+
+let resultado = area (4,5)
+console.log(resultado)
+
+
+
+function perimetro( lado1 , lado2, lado3){
+    return lado1+lado2+lado3
+}
+
+let resultado2 = perimetro(10,20,30)
+console.log (resultado2)*/
+
+/*const compra = (precio,cantidad) => {
+    let precioTotal = precio * cantidad
+
+    if (cantidad>=10 && cantidad<=cantidad){
+        precioTotal*=0.9
+    }else if(cantidad>=20){
+        precioTotal*= 0.8
+   }
+   return precioTotal;
+}
+
+let totalcompra = compra(5,10)
+
+console.log(totalcompra)*/
+
+/*
+function mayordedad(a){
+    if(a>=18){
+        return console.log("es mayor")
+    }else if(a<18){
+        return console.log("es menor")
+    }
+}
+
+let mensaje = mayordedad(15)
+console.log(mensaje)*/
+
+/*function verificarDia(dia){
+    switch(dia){
+        case 1: 
+            console.log("es un dia laboral");
+        break;
+
+        case 2:
+            console.log("es un dia laboral")
+
+        case 3: 
+        console.log("es un dia laboral")
+
+        case 4:
+            console.log("es un dia laboral")
+
+        case 5:
+            console.log("es un dia laboral")
+        
+        case 6:
+            console.log("es fin de semana")
+
+        case 7:
+            console.log("es fin de semana")
+
+    }
+}
+
+let numero = verificarDia(7)
+console.log(numero) 
+*/
+
+
+
+
---
+++ intro Js/main.js
@@ -0,0 +1,63 @@
+/*let myName = "Gaston"
+console.log(myName)
+
+let myLastName = "Alvarez"
+let myAge = 26
+let myPet="Thor"
+let petAge = 3
+let fullName = myName + myLastName
+let presentationText = `Soy ${myName} ${myLastName} tengo ${myAge} años mi perro se llama ${myPet} y tiene ${petAge} años`
+console.log(presentationText)
+
+let sumAges = myAge + petAge
+let subtractAges = myAge - petAge
+let productAges = myAge * petAge
+let divisionAges = myAge / petAge 
+
+const student = {
+    name: "Mario",
+    lastname: "Lopez",
+    age: 20,
+    genero: "M",
+    city: "buenos aires"
+}
+
+console.table(student)
+console.log(student.name)
+console.log(student.lastname)
+console.log(student.age)
+console.log(student.genero)
+console.log(student.city)
+
+const pet = {
+    nombre: "Thor",
+    edad: 3,
+    color: "black",
+    raza: "rotwailer",
+    peso: "30kg"
+}
+
+console.table(pet)
+console.log(pet.nombre)
+console.log(pet.edad)
+console.log(pet.color)
+console.log(pet.raza)
+console.log(pet.peso)
+
+const frutas = ["manzana" , "naranja" , "pera", "durazno" , "kiwi"]
+console.log(frutas)
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+console.log(frutas[3])
+
+ let age;
+ age = prompt("introduce tu edad")
+
+ if(age>=18){
+    let imAdult = age;
+    console.log("soy un adulto" + imAdult) 
+ }else{
+    console.log("soy menor")
+ }
+    */
